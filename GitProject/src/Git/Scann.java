a517fbd983297ab7318c50e646fe90556288433e
rupesh <rupesh@LAPTOP-NKNV2PDF>
2024-09-11 12:02:05 +0530

    show and display methods

--- GitProject/src/Git/Scann.java
+++ GitProject/src/Git/Scann.java
@@ -4,6 +4,9 @@ public  class Scann {
 	public void show() {
 		System.out.println("git project");
 	}
+	public void display() {
+		System.out.println("git project");
+	}
 
 	public static void main(String[] args) {
 		Scann a = new Scann();
